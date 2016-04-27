6f6f2a0b930f23d6064a2d576e021b3daf98859a
Paco Guevara <pacoguevaraa@gmail.com>
2016-04-27 00:56:05 -0500

    add new action controller for playlist and add validations for create

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -6,4 +6,22 @@ class PlaylistsController < ApplicationController
   def show
     @playlist = Playlist.find(params[:id])
   end
+
+  def new
+    @playlist = Playlist.new
+  end
+
+  def create
+    @playlist = Playlist.new(playlist_params)
+    if @playlist.save
+      redirect_to @playlist
+    else
+      render :new
+    end
+  end
+
+  private
+  def playlist_params
+    params.require(:playlist).permit(:name, :number_of_votes)
+  end
 end
--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,2 +1,3 @@
 class Playlist < ActiveRecord::Base
+  validates_presence_of :name, :number_of_votes 
 end
